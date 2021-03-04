8762dc36f571dc309210e3a2bdc9380e092340e8
Joshua-Troy Meneses <jtmenese@gmail.com>
2021-03-03 23:52:21 -0800

    Implemented dynamic programming version of fib numbers.

--- algorithms/fib.py
+++ algorithms/fib.py
@@ -5,3 +5,11 @@ def fib_recur(n): # Recursive version of the fibonacci sequence
         return 1
     else:
         return fib_recur(n-1) + fib_recur(n - 2) 
+
+def fib_dynamic(n): # DP version, using bottom up calculations with memoization
+    table = [0, 1]  # T(n) = O(n), only has to run up to n - 1 times to calculate the nth fib number.
+    i = 2
+    for i in range(2, n+1):
+        table.append(table[i-1] + table[i-2])
+
+    return table[n]
